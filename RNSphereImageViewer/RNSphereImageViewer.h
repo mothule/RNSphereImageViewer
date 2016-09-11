bb2b93fa8fd75ea19a64df6b689bc8794c243ad0
Motoki Kawakami <mothule.dev@gmail.com>
2016-09-11 16:10:26 +0900

    Initial commit

---
+++ RNSphereImageViewer/RNSphereImageViewer.h
@@ -0,0 +1,19 @@
+//
+//  RNSphereImageViewer.h
+//  RNSphereImageViewer
+//
+//  Created by mothule on 2016/09/11.
+//  Copyright © 2016年 mothule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNSphereImageViewer.
+FOUNDATION_EXPORT double RNSphereImageViewerVersionNumber;
+
+//! Project version string for RNSphereImageViewer.
+FOUNDATION_EXPORT const unsigned char RNSphereImageViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNSphereImageViewer/PublicHeader.h>
+
+
